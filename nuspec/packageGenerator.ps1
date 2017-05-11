8ce6255d0b851af62e8eab3b8ab3dce6544b06c0
MACK Mathieu <mathieu.mack@econocom.com>
2017-05-11 22:45:47 +0200

    Update packaging for nuget

--- nuspec/packageGenerator.ps1
+++ nuspec/packageGenerator.ps1
@@ -1,15 +1,16 @@
-
 write-host "**************************" -foreground "Cyan"
 write-host "*   Packaging to nuget   *" -foreground "Cyan"
 write-host "**************************" -foreground "Cyan"
 
-#$location  = "C:\Sources\NoSqlRepositories";
 $location  = $env:APPVEYOR_BUILD_FOLDER
 
 $locationNuspec = $location + "\nuspec"
 $locationNuspec
 	
 Set-Location -Path $locationNuspec
+
+write-host "Update the nuget.exe file" -foreground "DarkGray"
+.\NuGet update -self
 
 $strPath = $location + '\MvvX.Plugins.IOAuthClient\bin\Release\MvvX.Plugins.IOAuthClient.dll'
 
@@ -18,23 +19,14 @@ $ProductVersion = $VersionInfos.ProductVersion
 write-host "Product version : " $ProductVersion -foreground "Green"
 
 write-host "Packaging to nuget..." -foreground "Magenta"
-
-write-host "Update nuspec versions" -foreground "Green"
-
-write-host "Update nuspec versions MvvX.Plugins.IOAuthClient.nuspec" -foreground "DarkGray"
-$nuSpecFile =  $locationNuspec + '\MvvX.Plugins.IOAuthClient.nuspec'
-(Get-Content $nuSpecFile) | 
-Foreach-Object {$_ -replace "{BuildNumberVersion}", "$ProductVersion" } | 
-Set-Content $nuSpecFile
-
 write-host "Generate nuget packages" -foreground "Green"
 
-write-host "Generate nuget package for MvvX.Plugins.IOAuthClient.nuspec" -foreground "DarkGray"
-.\NuGet.exe pack MvvX.Plugins.IOAuthClient.nuspec
+write-host "Generate nuget package for MvvX.Plugins.OAuthClient.nuspec" -foreground "DarkGray"
+.\NuGet.exe pack MvvX.Plugins.OAuthClient.nuspec -Version $ProductVersion
 
 $apiKey = $env:NuGetApiKey
 	
 write-host "Publish nuget packages" -foreground "Green"
 
-write-host MvvX.Plugins.IOAuthClient.$ProductVersion.nupkg -foreground "DarkGray"
-.\NuGet push MvvX.Plugins.IOAuthClient.$ProductVersion.nupkg -Source https://www.nuget.org/api/v2/package -ApiKey $apiKey
+write-host MvvX.Plugins.OAuthClient.$ProductVersion.nupkg -foreground "DarkGray"
+.\NuGet push MvvX.Plugins.OAuthClient.$ProductVersion.nupkg -Source https://www.nuget.org/api/v2/package -ApiKey $apiKey
