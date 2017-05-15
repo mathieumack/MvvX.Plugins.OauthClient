0b6d8f793656d7b40706d198e398d72b2fd9f580
MACK Mathieu <mathieu.mack@econocom.com>
2017-05-15 22:15:12 +0200

    Fix nuspec configuration (Missing dll references and update Bootstrap.cs file for  Droid

--- nuspec/BootstrapContent/OAuthPluginBootstrap.cs.pp
+++ nuspec/BootstrapContent/OAuthPluginBootstrap.cs.pp
@@ -3,7 +3,7 @@ using MvvmCross.Platform.Plugins;
  namespace $rootnamespace$.Bootstrap
  {
     public class OAuthPluginBootstrap
-        : MvxPluginBootstrapAction<MvvX.Plugins.AssemblyFinder.PluginLoader>
+        : MvxPluginBootstrapAction<MvvX.Plugins.IOAuthClient.PluginLoader>
     {
     }
  }
